15d38ebfe184b5e330d439f93d314a9edacd0cbd
VorahPong <vorahpongmean@gmail.com>
2025-06-03 15:46:53 -0500

    Refactor CUCapstone Code

--- script.js
+++ script.js
@@ -15,56 +15,19 @@ const showMoreCUCapstone = document.getElementById('showMoreCUCapstone');
 showMoreCUCapstone.addEventListener("click", () => {
     showMoreCUCapstone.style.visibility = "hidden";
 
-    const CUCapstone = document.getElementById('CUCapstone');
-
-    const html = `
-    <div id="CUCapstoneExtraContent">
-
-      <div class="title2">
-        <h1 style="margin-left: auto; margin-right: auto">Progress</h1>
-      </div>
-      <div id="lineBreaker"></div>
-
-      <div class='progress'>
-      <img src="./assets/oldLogin.png" alt="Old Login Image" width="500px">
-      <img src="./assets/arrowIcon.png" alt="Arrow Image" width="80px" height="80px">
-      <img src="./assets/newLogin.png" alt="New Login Image" width="500px">
-      </div>
-
-      <div class='progress'>
-      <img src="./assets/capstoneProgOld.png" alt="Old Main Page" width="500px">
-      <img src="./assets/arrowIcon.png" alt="Arrow Image" width="80px" height="80px">
-      <img src="./assets/capstoneProgNew.png" alt="New Main Page" width="500px">
-      </div>
-
-      <ul style="margin-top: 80px;">
-        <li>The project was developed using HTML, CSS, PHP, JavaScript, SQL, and run on XAMPP.</li>
-        <li>Ensure that account passwords are securely hashed using the latest recommended PHP algorithms, such as password_hash() with PASSWORD_DEFAULT.</li>
-
-      </ul>
-
-      <div class="showMore" id="showLessCUCapstone">
-        <label>Show Less</label>
-        <img src="assets/showLessIcon.png" alt="Show More" width="50px" style="margin-bottom: 50px;">
-      </div>
-    </div>
-    `;
-
-    CUCapstone.insertAdjacentHTML('beforeend', html);
+    const CUCapstoneExtraContent = document.getElementById('CUCapstoneExtraContent');
+    CUCapstoneExtraContent.style.display = "block";
 
     const showLessCUCapstone = document.getElementById('showLessCUCapstone');
     showLessCUCapstone.addEventListener('click', () => {
-      const extraContent = document.getElementById('CUCapstoneExtraContent');
-      if (extraContent) {
-        extraContent.remove();
+        CUCapstoneExtraContent.style.display = "none";
         showMoreCUCapstone.style.visibility = "visible";
 
         // Scroll back
+        const CUCapstone = document.getElementById('CUCapstone');
         const CUCapstonePosition = CUCapstone.getBoundingClientRect().top + window.scrollY - 100;
         window.scrollTo(0, CUCapstonePosition);
-      }
-    });
-
+      });
 });
 
 //
